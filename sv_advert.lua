b5ea62914311eb3c8f2b69d611fb76131ad1f731
Florian_RVD <33377363+Tronpa@users.noreply.github.com>
2023-10-25 15:54:17 +0200

    Update sv_advert.lua

--- sv_advert.lua
+++ sv_advert.lua
@@ -1,72 +1,98 @@
-require("mysqloo")
+local useMySQL = true
 
-local dbInfo = {
-    host = "", 
-    port = 3306,
-    database = "",
-    user = "",
-    password = ""
-}
+local messages = {}
 
-local db = mysqloo.connect(dbInfo.host, dbInfo.user, dbInfo.password, dbInfo.database, dbInfo.port)
-
-function CreateMessagesTable()
-    if not db or not db:status() == mysqloo.DATABASE_CONNECTED then
-        print("Erreur de connexion à la base de données MySQL.")
-        return
+if useMySQL then
+    require("mysqloo")
+    
+    local dbInfo = {
+        host = "",
+        port = 3306,
+        database = "",
+        user = "",
+        password = ""
+    }
+    
+    local db = mysqloo.connect(dbInfo.host, dbInfo.user, dbInfo.password, dbInfo.database, dbInfo.port)
+    
+    function CreateMessagesTable()
+        if not db or not db:status() == mysqloo.DATABASE_CONNECTED then
+            print("Erreur de connexion à la base de données MySQL.")
+            return
+        end
+    
+        local query = db:query([[
+            CREATE TABLE IF NOT EXISTS messages (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                message TEXT
+            )
+        ]])
+    
+        query.onSuccess = function()
+            print("Table de messages créée avec succès.")
+        end
+    
+        query.onError = function(_, err)
+            print("Erreur MySQL : " .. err)
+        end
+    
+        query:start()
     end
+    
+    CreateMessagesTable()
+else
 
-    local query = db:query([[
-        CREATE TABLE IF NOT EXISTS messages (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            message TEXT
-        )
-    ]])
+    messages = {
+        "Message local 1",
+        "Message local 2",
+        "Message local 3"
 
-    query.onSuccess = function()
-        print("Table de messages créée avec succès.")
-    end
-
-    query.onError = function(_, err)
-        print("Erreur MySQL : " .. err)
-    end
-
-    query:start()
+    }
 end
-
-CreateMessagesTable()
 
 function LoadAndSendRandomMessage()
-    if not db or not db:status() == mysqloo.DATABASE_CONNECTED then
-        print("Erreur de connexion à la base de données MySQL.")
-        return
-    end
-
-    local query = db:query("SELECT message FROM messages ORDER BY RAND() LIMIT 1")
-
-    query.onSuccess = function(_, data)
-        if data and data[1] and data[1].message then
-            local randomMessage = data[1].message
-            local prefix = "[ProphuntZ] "
-
-            local prefixColor = Color(255, 0, 0) 
-            local messageColor = Color(255, 255, 255)
-
-            net.Start("SendRandomMessage")
-            net.WriteString(prefix)
-            net.WriteString(randomMessage)
-            net.Send(player.GetAll())
+    local randomMessage
+    
+    if useMySQL then
+        if not db or not db:status() == mysqloo.DATABASE_CONNECTED then
+            print("Erreur de connexion à la base de données MySQL.")
+            return
         end
+    
+        local query = db:query("SELECT message FROM messages ORDER BY RAND() LIMIT 1")
+    
+        query.onSuccess = function(_, data)
+            if data and data[1] and data[1].message then
+                randomMessage = data[1].message
+            end
+        end
+    
+        query.onError = function(_, err)
+            print("Erreur MySQL : " .. err)
+        end
+    
+        query:start()
+    else
+        local randomIndex = math.random(1, #messages)
+        randomMessage = messages[randomIndex]
     end
 
-    query.onError = function(_, err)
-        print("Erreur MySQL : " .. err)
-    end
+    if randomMessage then
+        local prefix = "[ProphuntZ] "
 
-    query:start()
+        local prefixColor = Color(255, 0, 0) 
+        local messageColor = Color(255, 255, 255)
+
+        net.Start("SendRandomMessage")
+        net.WriteString(prefix)
+        net.WriteString(randomMessage)
+        net.Send(player.GetAll())
+    end
 end
 
-db:connect()
+if useMySQL then
+    db:connect()
+end
 
 for i = 1, 5 do
     LoadAndSendRandomMessage(function(message)
